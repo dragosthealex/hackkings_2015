61009e1bd1d32cbb3db8d7fe2bfb82cdc628f38d
Geanina Hinta <geaninahinta@yahoo.com>
2015-12-12 23:42:02 +0000

    Updated the table insertion queries

--- server/sql/insert-tables.sql
+++ server/sql/insert-tables.sql
@@ -6,3 +6,111 @@ insert into Charities values (
 	"http://images.sandbox.justgiving.com/image/groupb_logo_large.gif",
 	"https://www.justgiving.com/groupb"
 );
+
+insert into Charities values (
+    2050,
+    "The Demo Charity1",
+    "We raise money to support the work of Great Ormond Street Hospital so that it can help more children who need its specialist expertise.",
+    "http://www.democharity.co.uk",
+    "http://images.sandbox.justgiving.com/image/76012844-e161-4bad-bb46-19bb9eafc1c5.jpg",
+    "https://www.justgiving.com/jgdemo"
+);
+
+insert into Charities values (
+    2074,
+    "Blue Cross",
+    "The Blue Cross provides practical support, information and advice for pet and horse owners. Through a network of animal adoption centres it rehomes thousands of animals each year. Its\nhospitals provide veterinary care for the pets of people who cannot afford private vets' fees. ",
+    "http://www.bluecross.org.uk",
+    "http://images.sandbox.justgiving.com/image/larche_logo.gif",
+    "https://www.justgiving.com/larche"
+);
+
+insert into Charities values (
+    2077,
+    "L'Arche",
+    "L'Arche builds communities with people who have learning disabilities. Together with assistants, they live in ordinary houses, with choice in and access to work, therapy and local resources. The nine L'Arche communities in England, Scotland and Wales are founded on Christian values, also welcoming people of other faiths and none.",
+    "http://www.larche.org.uk",
+    "http://images.sandbox.justgiving.com/image/larche_logo.gif",
+    "https://www.justgiving.com/larche"
+);
+
+insert into Charities values (
+    100,
+    "Sense - National Deafblind and Rubella Association",
+    "Sense is the UK's largest organisation offering support to deafblind people and their families.  Established in 1955 as a parent's support group, Sense now offers a range of services - including, advice, housing, education and practical help to children and adults who are both deaf and blind.",
+    "http://www.sense.org.uk",
+    "http://images.sandbox.justgiving.com/image/sense_logo.gif",
+    "https://www.justgiving.com/sense"
+);
+
+insert into Charities values (
+    102,
+    "Karuna Trust",
+    "Karuna supports educational, health and cultural projects throughout South Asia that are helping thousands of oppressed women, men and children to develop the skills, dignity and confidence to transform their lives and take their rightful place in society.",
+    "http://www.karuna.org",
+    "http://images.sandbox.justgiving.com/image/karuna_logo.gif",
+    "https://www.justgiving.com/karuna"
+);
+
+insert into Charities values (
+    2002,
+    "Breast Cancer Campaign",
+    "At Breast Cancer Campaign, everyone can be part of the cure. We are a unique community of scientists, supporters and people touched by breast cancer. Together we make Pink Science happen – our pioneering approach to breast cancer research that identifies and funds the best research projects. ",
+    "http://www.breastcancercampaign.org",
+    "http://images.sandbox.justgiving.com/image/85e0e6db-eada-4638-a97d-6dcbcec91835.gif",
+    "https://www.justgiving.com/breastcancer"
+);
+
+insert into Charities values (
+    2010,
+    "Andy Cole Children's Foundation",
+    "The Andy Cole Children's Foundation was set up by the world famous England and Blackburn Rovers football star to help disadvantaged children, primarily in countries of the Southern African Region but also all over the world and the UK.",
+    "http://www.andycolecf.com",
+    "http://images.sandbox.justgiving.com/image/andycole_child_logo_large.gif",
+    "https://www.justgiving.com/andycole"
+);
+
+insert into Charities values (
+    2013,
+    "NSPCC",
+    "The NSPCC's purpose is to end cruelty to children and our vision is of a Society where all children are loved, valued and able to fulfil their potential. We campaign to change the law, provide ChildLine and the NSPCC Helpline, offer advice for adults, and much more.  ",
+    "http://www.nspcc.org.uk",
+    "http://images.sandbox.justgiving.com/image/nspcc_logo_large2.gif",
+    "https://www.justgiving.com/nspcc"    
+);
+
+insert into Charities values (
+    2021,
+    "UK for UNHCR",
+    "UNHCR is the world's leading refugee agency which provides all-round assistance to those who are  forced to flee their homes - medical care, water, food, shelter and blankets. UK for UNHCR is the British charity that supports the work of UNHCR. It works to raise awareness of the needs of refugees and to raise funds to support programmes for refugees around the world.",
+    "http://www.ukforunhcr.org",
+    "http://images.sandbox.justgiving.com/image/unhcr_logo_large.gif",
+    "https://www.justgiving.com/refAid1"
+);
+
+insert into Charities values (
+    202,
+    "The European Children's Trust",
+    "The European Children’s Trust promotes reformed childcare systems in central and eastern Europe and the former Soviet Union with an aim of meeting the right and need of every child to grow up in a family environment.",
+    "http://www.everychild.org.uk",
+    "http://images.sandbox.justgiving.com/image/ect_logo_white.gif",
+    "https://www.justgiving.com/ect"
+);
+
+insert into Charities values (
+    2028,
+    "British Deaf Association",
+    "The British Deaf Association promotes funding for deaf people.",
+    "http://www.bda.org.uk",
+    "http://images.sandbox.justgiving.com/image/WLBDA_logo_large_WL.gif",
+    "https://www.justgiving.com/WLBDA"
+);
+
+insert into Charities values (
+    2029,
+    "Positively Women",
+    "Positively Women is the leading UK charity providing peer support - support for women living with HIV by women living with HIV. Positively Women aims to enable women living with HIV to make informed choices and to ensure that the voice of HIV positive women is heard.",
+    "http://www.positivelywomen.org.uk",
+    "http://images.sandbox.justgiving.com/image/positivelywomen_logo_large.gif",
+    "https://www.justgiving.com/positivelywomen old account"
+);
