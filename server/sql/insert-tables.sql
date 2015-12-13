125f474872250f0745664cc2a0052fffc1ab6b83
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-12-13 03:17:39 +0000

    Revert "Updated sql queries"
    This reverts commit 68318274bc8efbd9ecb33bfc30a9a6894c84ba3a.

--- server/sql/insert-tables.sql
+++ server/sql/insert-tables.sql
@@ -1,10 +1,10 @@
 insert into Charities values (
-    2069,
-    "Sample Reporting Charity",
-    "Group B Strep is the most common cause of life-threatening infection in newborn babies in the UK - infections that are mostly preventable. GBSS is a national charity providing information on GBS to families and health professionals, and offering support. It also raises funds for research.",
-    "http://www.gbss.org.uk",
-    "http://images.sandbox.justgiving.com/image/groupb_logo_large.gif",
-    "https://www.justgiving.com/groupb"
+	2069,
+	"Sample Reporting Charity",
+	"Group B Strep is the most common cause of life-threatening infection in newborn babies in the UK - infections that are mostly preventable. GBSS is a national charity providing information on GBS to families and health professionals, and offering support. It also raises funds for research.",
+	"http://www.gbss.org.uk",
+	"http://images.sandbox.justgiving.com/image/groupb_logo_large.gif",
+	"https://www.justgiving.com/groupb"
 );
 
 insert into Charities values (
@@ -121,7 +121,7 @@ insert into Cities values (
 );
 
 insert into Cities values (
-    2,
+    1,
     "Manchester"
 );
 
